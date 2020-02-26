9d718db3c4f3bf36c8ea6e3d735833fe56d62fcb
davideworldes <daviddo@eworldes.com>
2020-02-25 15:31:45 -1000

    client and server first commit

---
+++ server/src/main/java/io/letthegamesbegin/server/LetthegamesbeginApplication.java
@@ -0,0 +1,13 @@
+package io.letthegamesbegin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetthegamesbeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetthegamesbeginApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/letthegamesbegin/server/LetthegamesbeginApplicationTests.java
@@ -0,0 +1,13 @@
+package io.letthegamesbegin.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetthegamesbeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
