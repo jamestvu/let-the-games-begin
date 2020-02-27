c737badad8412253a9ea97183eca556a67ee93b0
David Do <david@daviddo.dev>
2020-02-26 23:17:53 -1000

    created a quick signup page layout. still needs to adjust size and layout.

--- client/src/pages/Signup.vue
+++ client/src/pages/Signup.vue
@@ -1,11 +1,32 @@
 <template>
   <q-page>
+    <q-form @submit="onSubmit">
+      <div>
+        <q-input type="text" label="FIRST NAME" dense />
+        <q-input type="text" label="LAST NAME" dense />
+        <q-input type="email" label="EMAIL" dense />
+        <q-input type="text" label="USERNAME" dense />
+        <q-input type="password" label="PASSWORD" dense />
+      </div>
+      <div>
+        <q-input type="text" label="ADDRESS" dense />
+        <q-input type="text" label="CITY" dense />
+        <q-btn-dropdown label="STATE" dense />
+        <q-input type="number" label="ZIP" dense />
+      </div>
+      <q-btn type="submit" label="Submit" />
+    </q-form>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'signup',
+  methods: {
+    onSubmit() {
+      console.log('signed up');
+    },
+  },
 };
 </script>
 
