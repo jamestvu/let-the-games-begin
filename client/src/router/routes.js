74f1cb3f8c05aa5ac49da4a13ea0dc238374e720
David Do <david@daviddo.dev>
2020-02-26 23:00:49 -1000

    added new pages for the navigation when users arent logged in. created a new folder in the pages folder called loggedInPages which shows pages when they are logged in. created home.vue for when they have logged in. updated routes.js to point the navigation to their respected pages.

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,6 +5,10 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
+      { path: '/events', component: () => import('pages/Events.vue') },
+      { path: '/ranks', component: () => import('pages/Ranks.vue') },
+      { path: '/pricing', component: () => import('pages/Pricing.vue') },
+      { path: '/signup', component: () => import('pages/Signup.vue') },
     ],
   },
 ];
